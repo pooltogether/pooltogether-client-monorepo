d02ffe663d5a96f079e77bc08b17e52db4d88e0d
Ncookiez <ncukiert@gmail.com>
2023-02-13 10:55:34 -0300

    Added Sorting Utils

--- packages/pt-utilities/src/math.ts
+++ packages/pt-utilities/src/math.ts
@@ -27,3 +27,29 @@ export const calculatePercentageOfBigNumber = (
 ) => {
   return bn.mul(percentage * 10 ** precision).div(10 ** precision)
 }
+
+/**
+ * Helper function to sort BigNumbers in ascending order
+ * @param a first BigNumber
+ * @param b second BigNumber
+ * @returns
+ */
+export const sortByBigNumberAsc = (a: BigNumber, b: BigNumber) => {
+  const aSubB = a.sub(b)
+  if (aSubB.isZero()) return 0
+  if (aSubB.isNegative()) return -1
+  return 1
+}
+
+/**
+ * Helper function to sort BigNumbers in descending order
+ * @param a first BigNumber
+ * @param b second BigNumber
+ * @returns
+ */
+export const sortByBigNumberDesc = (a: BigNumber, b: BigNumber) => {
+  const bSubA = b.sub(a)
+  if (bSubA.isZero()) return 0
+  if (bSubA.isNegative()) return -1
+  return 1
+}
