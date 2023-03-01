5c1c7e84f64da317b29503b1b6445538aa54d7cb
Ncookiez <ncukiert@gmail.com>
2023-03-01 17:44:05 -0300

    Fixed NetworkIcon

--- packages/pt-components/src/components/NetworkIcon.tsx
+++ packages/pt-components/src/components/NetworkIcon.tsx
@@ -1,6 +1,9 @@
 import classNames from 'classnames'
-import { getNiceNetworkNameByChainId } from 'pt-utilities'
-import { NETWORK_ICONS } from '../constants'
+import { NETWORK } from 'pt-utilities'
+import { ArbitrumIcon } from '../icons/arbitrum'
+import { EthereumIcon } from '../icons/ethereum'
+import { OptimismIcon } from '../icons/optimism'
+import { PolygonIcon } from '../icons/polygon'
 import { FallbackIcon } from './FallbackIcon'
 
 export interface NetworkIconProps {
@@ -11,17 +14,21 @@ export interface NetworkIconProps {
 export const NetworkIcon = (props: NetworkIconProps) => {
   const { chainId, className } = props
 
-  const networkName = getNiceNetworkNameByChainId(chainId)
-
-  if (chainId in NETWORK_ICONS) {
-    return (
-      <img
-        src={NETWORK_ICONS[chainId as keyof typeof NETWORK_ICONS].iconUrl}
-        alt={`${networkName} Logo`}
-        className={classNames('h-6 w-6', className)}
-      />
-    )
+  if (chainId in icons) {
+    const SvgIcon = icons[chainId as keyof typeof icons].svgIcon
+    return <SvgIcon className={classNames('h-6 w-6 rounded-full', className)} />
   }
 
   return <FallbackIcon symbol={''} />
 }
+
+const icons = Object.freeze({
+  [NETWORK.mainnet]: { svgIcon: EthereumIcon, iconBgColor: '#484c50' },
+  [NETWORK.goerli]: { svgIcon: EthereumIcon, iconBgColor: '#484c50' },
+  [NETWORK.polygon]: { svgIcon: PolygonIcon, iconBgColor: '#9f71ec' },
+  [NETWORK.mumbai]: { svgIcon: PolygonIcon, iconBgColor: '#9f71ec' },
+  [NETWORK.optimism]: { svgIcon: OptimismIcon, iconBgColor: '#ff5a57' },
+  [NETWORK['optimism-goerli']]: { svgIcon: OptimismIcon, iconBgColor: '#ff5a57' },
+  [NETWORK.arbitrum]: { svgIcon: ArbitrumIcon, iconBgColor: '#96bedc' },
+  [NETWORK['arbitrum-goerli']]: { svgIcon: ArbitrumIcon, iconBgColor: '#96bedc' }
+})
--- packages/pt-components/src/index.tsx
+++ packages/pt-components/src/index.tsx
@@ -4,8 +4,3 @@
 export * from './components/FallbackIcon'
 export * from './components/NetworkIcon'
 export * from './components/TokenIcon'
-
-/**
- * Constants
- */
-export * from './constants'
