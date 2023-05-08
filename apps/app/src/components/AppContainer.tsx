ec05da2bbba25659d7132762208de2389420b1d6
Ncookiez <ncukiert@gmail.com>
2023-05-08 13:35:15 -0400

    Formatted Lockfile + Added JSDoc

--- apps/app/src/components/AppContainer.tsx
+++ apps/app/src/components/AppContainer.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from '@sentry/nextjs'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
