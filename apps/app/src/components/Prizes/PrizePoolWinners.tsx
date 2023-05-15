33913b3e524a53689d4744b28771500dc9b0faa1
Ncookiez <ncukiert@gmail.com>
2023-05-15 17:49:53 -0400

    Draw Winners Row Styling

--- apps/app/src/components/Prizes/PrizePoolWinners.tsx
+++ apps/app/src/components/Prizes/PrizePoolWinners.tsx
@@ -19,7 +19,7 @@ export const PrizePoolWinners = () => {
     return (
       <div className='flex flex-col w-[36rem] gap-4 items-center px-11 py-8 bg-pt-transparent rounded-lg'>
         <span className='text-xl font-semibold'>Recent Prize Pool Winners</span>
-        <ul className='flex flex-col w-full gap-4 px-3 py-2'>
+        <ul className='flex flex-col w-full'>
           {draws.slice(0, numDraws).map((draw) => {
             return <DrawRow key={`dr-${draw.id}`} draw={draw} prizePool={selectedPrizePool} />
           })}
@@ -42,7 +42,6 @@ interface DrawRowProps {
   prizePool: PrizePool
 }
 
-// TODO: row hover effect
 // TODO: open draw info modal when row clicked
 const DrawRow = (props: DrawRowProps) => {
   const { draw, prizePool } = props
@@ -53,7 +52,7 @@ const DrawRow = (props: DrawRowProps) => {
   const totalPrizeAmount = draw.prizeClaims.reduce((a, b) => a.add(b.payout), BigNumber.from(0))
 
   return (
-    <div className='inline-flex gap-4 justify-between font-semibold text-pt-purple-100 whitespace-nowrap'>
+    <div className='inline-flex gap-4 justify-between px-3 py-2 font-semibold text-pt-purple-100 rounded-lg cursor-pointer whitespace-nowrap hover:bg-pt-transparent'>
       <span>Draw #{draw.id}</span>
       {!!tokenData && (
         <span className='inline-flex gap-2'>
