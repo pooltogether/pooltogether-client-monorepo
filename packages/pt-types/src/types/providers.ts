9334a4b58e86479337ac2e41036019348ac037e3
Ncookiez <ncukiert@gmail.com>
2023-05-19 16:29:04 -0400

    Pkg + Tsconfig + Readme Changes

--- packages/pt-types/src/types/providers.ts
+++
@@ -1,9 +0,0 @@
-import type { providers, Signer } from 'ethers'
-
-export interface Providers {
-  [chainId: number]: providers.Provider
-}
-
-export interface SignersOrProviders {
-  [chainId: number]: Signer | providers.Provider
-}
