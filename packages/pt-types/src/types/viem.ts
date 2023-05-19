3e1c0700ebeb4de0fc2c37f1fdc976f389cec7b2
Ncookiez <ncukiert@gmail.com>
2023-05-19 17:48:22 -0400

    Types Cleanup

--- packages/pt-types/src/types/viem.ts
+++
@@ -1,9 +0,0 @@
-import type { PublicClient, WalletClient } from 'viem'
-
-export interface ViemPublicClients {
-  [chainId: number]: PublicClient
-}
-
-export interface ViemWalletClients {
-  [chainId: number]: WalletClient
-}
