3af450401c8b1188f3e4049d7d5333b06fb483ce
Ncookiez <ncukiert@gmail.com>
2023-06-24 17:34:18 -0400

    Added Missing Messages

--- apps/landing-page/src/pages/terms.tsx
+++ apps/landing-page/src/pages/terms.tsx
@@ -1,5 +1,20 @@
+import { SECONDS_PER_DAY } from '@shared/utilities'
+import { GetStaticProps } from 'next'
 import { Layout } from '@components/Layout'
 import { TermsOfService } from '@components/TermsOfService'
+
+interface TermsPageProps {
+  messages: IntlMessages
+}
+
+export const getStaticProps: GetStaticProps<TermsPageProps> = async ({ locale }) => {
+  const messages: IntlMessages = (await import(`../../messages/${locale}.json`)).default
+
+  return {
+    props: { messages },
+    revalidate: SECONDS_PER_DAY
+  }
+}
 
 export default function TermsPage() {
   return (
