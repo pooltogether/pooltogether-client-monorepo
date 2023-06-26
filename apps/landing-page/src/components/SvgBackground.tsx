57791df36e316e70f4ef0f505072e0d934b583ea
Ncookiez <ncukiert@gmail.com>
2023-06-26 18:29:16 -0400

    Pre-Rendering BGs

--- apps/landing-page/src/components/SvgBackground.tsx
+++ apps/landing-page/src/components/SvgBackground.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { useReducedMotion } from 'framer-motion'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 export interface SvgBackgroundProps {
@@ -21,47 +22,73 @@ export const SvgBackground = (props: SvgBackgroundProps) => {
     }
   }, [])
 
+  const isBgAnimated = !!animatedBg && !isReducedMotion
+  const isSmallBgAnimated = !!animatedSmallBg && !isReducedMotion
+
+  const bgSrc = `/backgrounds/static/${bg}`
+  const smallBgSrc = `/backgrounds/static/${smallBg}`
+  const animatedBgSrc = `/backgrounds/animated/${animatedBg}`
+  const animatedSmallBgSrc = `/backgrounds/animated/${animatedSmallBg}`
+
   const baseClassName = 'absolute w-full -z-10'
   const bgClassName = 'hidden md:block'
   const smallBgClassName = 'md:hidden'
 
-  const bgSrc = `/backgrounds/static/${bg}`
-  const smallBgSrc = `/backgrounds/static/${smallBg}`
-  const animatedBgSrc = animatedBg ? `/backgrounds/animated/${animatedBg}` : bgSrc
-  const animatedSmallBgSrc = animatedSmallBg
-    ? `/backgrounds/animated/${animatedSmallBg}`
-    : smallBgSrc
-
   return (
     <>
-      {isReducedMotion && (
-        <>
-          <object
-            type='image/svg+xml'
-            data={bgSrc}
-            className={classNames(baseClassName, bgClassName)}
-          />
-          <object
-            type='image/svg+xml'
-            data={smallBgSrc}
-            className={classNames(baseClassName, smallBgClassName)}
-          />
-        </>
+      <StaticBG
+        src={isBgAnimated ? animatedBgSrc : bgSrc}
+        className={classNames(baseClassName, bgClassName)}
+      />
+      <StaticBG
+        src={isSmallBgAnimated ? animatedSmallBgSrc : smallBgSrc}
+        className={classNames(baseClassName, smallBgClassName)}
+        isSmall={true}
+      />
+      {isBgAnimated && (
+        <AnimatedBG
+          src={animatedBgSrc}
+          className={classNames(baseClassName, bgClassName, '-z-[5]')}
+        />
       )}
-      {!isReducedMotion && (
-        <>
-          <object
-            type='image/svg+xml'
-            data={animatedBgSrc}
-            className={classNames(baseClassName, bgClassName)}
-          />
-          <object
-            type='image/svg+xml'
-            data={animatedSmallBgSrc}
-            className={classNames(baseClassName, smallBgClassName)}
-          />
-        </>
+      {isSmallBgAnimated && (
+        <AnimatedBG
+          src={animatedSmallBgSrc}
+          className={classNames(baseClassName, smallBgClassName, '-z-[5]')}
+        />
       )}
     </>
   )
+}
+
+interface StaticBGProps {
+  src: string
+  isSmall?: boolean
+  className?: string
+}
+
+const StaticBG = (props: StaticBGProps) => {
+  const { src, isSmall, className } = props
+
+  return (
+    <Image
+      src={src}
+      width={isSmall ? 375 : 1920}
+      height={isSmall ? 667 : 1080}
+      alt='Static BG'
+      className={className}
+      priority={true}
+    />
+  )
+}
+
+interface AnimatedBGProps {
+  src: string
+  className?: string
+}
+
+const AnimatedBG = (props: AnimatedBGProps) => {
+  const { src, className } = props
+
+  return <object type='image/svg+xml' data={src} className={className} />
 }
