efa7e032dd64a3b01e3c1c769b656873cfcbae2d
Ncookiez <ncukiert@gmail.com>
2023-06-26 13:45:36 -0400

    Fixed Head

--- apps/landing-page/src/pages/_document.tsx
+++ apps/landing-page/src/pages/_document.tsx
@@ -23,43 +23,42 @@ class MyDocument extends Document {
 
     return (
       <Html className='bg-pt-bg-purple-darker text-gray-100 overflow-x-hidden dark'>
-        <title>{title}</title>
-
-        <link rel='icon' href='/favicon.png' type='image/x-icon' />
-        <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-
-        <meta name='theme-color' content='#21064e' />
-        <meta name='description' content={description} />
-        <meta name='keywords' content={keywords} />
-        <meta name='author' content='PT Inc.' />
-
-        <meta property='og:title' content={title} />
-        <meta property='og:description' content={description} />
-        <meta property='og:site_name' content={title} />
-        <meta property='og:url' content={DOMAINS.landingPage} />
-        <meta property='og:type' content='website' />
-        <meta
-          property='og:image'
-          content={`${DOMAINS.landingPage}/pooltogether-facebook-share-image-1200-630@2x.png`}
-        />
-        <meta property='og:rich_attachment' content='true' />
-        <meta property='og:image:width' content='1200' />
-        <meta property='og:image:height' content='630' />
-
-        <meta property='twitter:title' content={title} />
-        <meta property='twitter:description' content={description} />
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:site' content={twitterHandle} />
-        <meta
-          property='twitter:image:src'
-          content={`${DOMAINS.landingPage}/pooltogether-twitter-share-image-1200-675@2x.png`}
-        />
-        <meta property='twitter:url' content={DOMAINS.landingPage} />
-        <meta property='twitter:creator' content={twitterHandle} />
-
-        <link rel='manifest' href='/manifest.json' />
         <Head>
-          <link rel='icon' href='/favicon.ico' />
+          <title>{title}</title>
+
+          <link rel='icon' href='/favicon.png' type='image/x-icon' />
+          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+
+          <meta name='theme-color' content='#21064e' />
+          <meta name='description' content={description} />
+          <meta name='keywords' content={keywords} />
+          <meta name='author' content='PT Inc.' />
+
+          <meta property='og:title' content={title} />
+          <meta property='og:description' content={description} />
+          <meta property='og:site_name' content={title} />
+          <meta property='og:url' content={DOMAINS.landingPage} />
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:image'
+            content={`${DOMAINS.landingPage}/pooltogether-facebook-share-image-1200-630@2x.png`}
+          />
+          <meta property='og:rich_attachment' content='true' />
+          <meta property='og:image:width' content='1200' />
+          <meta property='og:image:height' content='630' />
+
+          <meta property='twitter:title' content={title} />
+          <meta property='twitter:description' content={description} />
+          <meta property='twitter:card' content='summary_large_image' />
+          <meta property='twitter:site' content={twitterHandle} />
+          <meta
+            property='twitter:image:src'
+            content={`${DOMAINS.landingPage}/pooltogether-twitter-share-image-1200-675@2x.png`}
+          />
+          <meta property='twitter:url' content={DOMAINS.landingPage} />
+          <meta property='twitter:creator' content={twitterHandle} />
+
+          <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
           <Main />
